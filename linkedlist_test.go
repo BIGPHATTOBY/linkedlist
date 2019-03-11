869491cb443fe307d9fd18ac0d59ac7757ad03f5
Tobias Brodersen <twabias@gmail.com>
2019-03-11 22:20:26 +0100

    tests

--- linkedlist_test.go
+++ linkedlist_test.go
@@ -57,3 +57,69 @@ func TestGetValueByIndex(t *testing.T) {
 		t.Error("Expected baz, got ", s)
 	}
 }
+
+func TestAppend(t *testing.T) {
+	ll := NewLinkedList()
+	ll.Append("foo")
+	length, err := ll.Length()
+	if err != nil {
+		println(err)
+	}
+	if length != 1 {
+		t.Error("Expected 1, got ", length)
+	}
+	ll.Append("bar")
+	length, err = ll.Length()
+	if err != nil {
+		println(err)
+	}
+	if length != 2 {
+		t.Error("Expected 2, got ", length)
+	}
+}
+
+func TestInsert(t *testing.T) {
+	ll := NewLinkedList()
+	ll.Insert("foo", 0)
+	length, err := ll.Length()
+	if err != nil {
+		t.Error(err)
+	}
+	if length != 1 {
+		t.Error("Expected length to be 1, got ", length)
+	}
+	s, _ := ll.GetValueByIndex(1)
+	if s != "foo" {
+		t.Error("Expected \"foo\", got ", s)
+	}
+	ll.Insert("bar", 0)
+	s, _ = ll.GetValueByIndex(1)
+	if s != "bar" {
+		t.Error("Expected \"bar\", got ", s)
+	}
+	s, _ = ll.GetValueByIndex(2)
+	if s != "foo" {
+		t.Error("Expected \"foo\", got ", s)
+	}
+}
+
+func TestDelete(t *testing.T) {
+	ll := NewLinkedList()
+	ll.Append("foo")
+	ll.Append("bar")
+	length, err := ll.Length()
+	if err != nil {
+		t.Error(err)
+	}
+	if length != 2 {
+		t.Error("Expected length to be 2, got ", length)
+	}
+	ll.Delete(1)
+	length, err = ll.Length()
+	if err != nil {
+		t.Error(err)
+	}
+	if length != 1 {
+		t.Error("Expected length to be 1, got ", length)
+	}
+}
